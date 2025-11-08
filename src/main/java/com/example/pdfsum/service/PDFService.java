75690e7c31c698230eafd311633eaa0fcf8c0166
nikita-sarkar <nikita2019sarkar@gmail.com>
2025-11-09 00:24:29 +0530

    Created controller

---
+++ src/main/java/com/example/pdfsum/service/PDFService.java
@@ -0,0 +1,11 @@
+package com.example.pdfsum.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class PDFService {
+
+    private String processPDF(MultipartFile file)
+    {
+        return  "";
+    }
+}
